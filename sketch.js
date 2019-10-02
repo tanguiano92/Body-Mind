eefeec0378638b605c940153dc81477f940973c3
Toni Anguiano <toni.anguiano@sjsu.edu>
2019-10-02 16:10:50 -0700

    updates to Sketch and HTML

--- sketch.js
+++ sketch.js
@@ -1,15 +1,45 @@
+var hue;
+
 function setup() {
   createCanvas(600, 600);
-  background(220);
+background(96, 165, 240);
 
-  strokeWeight (8);
+strokeWeight(5);
+
+
+
+hue = 0;
 }
 
 function draw() {
 
-  if (mouseIsPressed){
-    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
-    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
-  }
+  //if (mouseIsPressed){
+    //strokeWeight(5);
+    //line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    //line(mouseX, mouseY, pmouseX, pmouseY);
+  //}
 
+}
+
+function keyTyped(){
+
+  console.log(`key ${key} s is being typed`)
+
+if (key === 's'){
+// save this image
+saveCanvas('fileName', 'png');
+
+}
+
+return false;
+
+}
+
+function mouseDragged() {
+  hue ++;
+  colorMode(HSL, 360);
+     noStroke();
+    fill(hue, 200, 200);
+    ellipse(width - mouseX, height - mouseY, 20, 20);
+    ellipse(mouseX, mouseY, 20, 20);
 }
