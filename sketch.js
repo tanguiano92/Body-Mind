c69a2b1b25268f38c226ae443b56c68077bf6b9b
tanguiano92 <47006589+tanguiano92@users.noreply.github.com>
2019-10-09 16:33:37 -0700

    Name Update/Perlin Noise

--- sketch.js
+++ sketch.js
@@ -1,25 +1,53 @@
-var hue;
+let noiseOffset = 0.0;
+//var hue;
+
+let strokeWidth = 5;
+
+let array = [];
 
 function setup() {
-  createCanvas(600, 600);
-background(96, 165, 240);
-
-strokeWeight(5);
+  createCanvas(windowWidth, windowHeight);
+background(0);
 
 
 
-hue = 0;
+
+
+//hue = 0;
 }
 
 function draw() {
+  background(120, 8, 12, 5);
 
-  //if (mouseIsPressed){
-    //strokeWeight(5);
-    //line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
-    //line(mouseX, mouseY, pmouseX, pmouseY);
-  //}
+strokeWeight(strokeWidth);
+
+noiseOffset += 0.05;
+strokeWidth = noise(noiseOffset) * 100;
+
+stroke(map(mouseX, 122, 5, 9, true))
+line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+line(mouseX, mouseY, pmouseX, pmouseY);
 
 }
+
+//function mouseDragged() {
+  //strokeWeight(strokeWidth);
+
+  //noiseOffset += 0.05;
+  //noiseOffset = noise(noiseOffset) * 100;
+
+  //if (mouseDragged){
+    //ellipse(mouseX, mouseY, 10, 10);
+    //array.push([mouseX, mouseY]);
+  //}
+
+  //hue ++;
+  //colorMode(HSL, 360);
+     //stroke(map(mouseX, hue, 200, 200));
+    //fill(hue, 200, 200);
+    //ellipse(width - mouseX, height - mouseY, 10, 10);
+    //ellipse(mouseX, mouseY, 10, 10);
+//}
 
 function keyTyped(){
 
@@ -29,17 +57,18 @@ if (key === 's'){
 // save this image
 saveCanvas('fileName', 'png');
 
+} else if (key === 'b'){
+  //display image
+
+for(let i = 0; i < array.length - 1; i ++){
+ellipse(array[0][0], array[0][1],array[i + 1][0], array[i + 1][1]);
+
+}
+
+
+
 }
 
 return false;
 
-}
-
-function mouseDragged() {
-  hue ++;
-  colorMode(HSL, 360);
-     noStroke();
-    fill(hue, 200, 200);
-    ellipse(width - mouseX, height - mouseY, 20, 20);
-    ellipse(mouseX, mouseY, 20, 20);
 }
