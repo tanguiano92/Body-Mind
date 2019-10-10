a6231dd83a91c854a68811cf4126cb3cad8d6c3e
tanguiano92 <47006589+tanguiano92@users.noreply.github.com>
2019-10-10 15:37:08 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,44 +7,8 @@ let array = [];
 
 
 function setup() {
-
-  //hue = 0;
-}
-
-
-function draw() {
   createCanvas(windowWidth, windowHeight);
   background(0);
-  if (mouseIsPressed){
-   line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
-   line(mouseX, mouseY, pmouseX, pmouseY);
-    array.push([mouseX, mouseY]);
-  }
-  strokeWeight(strokeWidth);
-
-  noiseOffset += 0.05;
-  strokeWidth = noise(noiseOffset) * 100;
-
-  }
-
-  function keyTyped() {
-
-    console.log(`key ${key} s is being typed`)
-
-    if (key === 's') {
-      // save this image
-      saveCanvas('fileName', 'png');
-
-    } else if (key === 'c') {
-      //clear image
-      clear();
-    } else if (key === 'd') {
-     // pulse image
-     for (let i = 0; i < array.length - 1; i++) {
-     line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
-    }
-
-      }
 
 
   stroke(82, 2, 2);
@@ -66,8 +30,42 @@ function draw() {
       }
     }
   }
+  //hue = 0;
+}
 
+
+function draw() {
   background(0, 0, 0, 3);
+if (mouseIsPressed){
+ line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+ line(mouseX, mouseY, pmouseX, pmouseY);
+  array.push([mouseX, mouseY]);
+}
+strokeWeight(strokeWidth);
+
+noiseOffset += 0.05;
+strokeWidth = noise(noiseOffset) * 100;
+
+}
+
+function keyTyped() {
+
+  console.log(`key ${key} s is being typed`)
+
+  if (key === 's') {
+    // save this image
+    saveCanvas('fileName', 'png');
+
+  } else if (key === 'c') {
+    //clear image
+    clear();
+  } else if (key === 'd') {
+   // display image
+   for (let i = 0; i < array.length - 1; i++) {
+   line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+  }
+
+    }
 
 
 
