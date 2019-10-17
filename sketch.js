a8395301ee3923716f74e567daf626f1e8094d5c
tanguiano92 <47006589+tanguiano92@users.noreply.github.com>
2019-10-17 15:22:41 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 let noiseOffset = 0.0;
 //var hue;
 
-let strokeWidth = 5;
+let strokeWidth = 1;
 
 let array = [];
 
@@ -10,26 +10,28 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
 
+  stroke(3, 3, 64);
+  for (let j = 0; j < 10; j ++){
+   for (let i = 0; i < 10; i ++){
+  line (j * 150, 0, windowWidth, i * 150);
+  }
 
-  stroke(82, 2, 2);
-
-    for (let j = 0; j < 10; j ++){
-     for (let i = 0; i < 10; i ++){
-    line (j * 150, 0, windowWidth, i * 150);
-    }
-
-       for (let j = 0; j < 10; j ++){
-     for (let i = 0; i < 10; i ++){
-    line (j * 150, windowWidth, 0, i * 150);
-    }
+     for (let j = 0; j < 10; j ++){
+   for (let i = 0; i < 10; i ++){
+  line (j * 150, windowWidth, 0, i * 150);
   }
 }
+}
+
+
+ angleMode(DEGREES);
   }
   //hue = 0;
 
 
 function draw() {
   background(0, 0, 0, 3);
+
 if (mouseIsPressed){
  line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
  line(mouseX, mouseY, pmouseX, pmouseY);
@@ -37,8 +39,49 @@ if (mouseIsPressed){
 }
 strokeWeight(strokeWidth);
 
-noiseOffset += 0.05;
+noiseOffset += 0.03;
 strokeWidth = noise(noiseOffset) * 100;
+
+
+
+translate(200, 200);
+ rotate(-90);
+
+ let hr = hour();
+ let min = minute();
+ let sec = second();
+
+  strokeWeight(strokeWidth);
+ noFill();
+ stroke(77, 4, 9);
+ let end1 = map(sec, 0, 60, 0, 360);
+ arc(0, 0, 500, 500, 0, end1);
+
+ push();
+ rotate(end1);
+ stroke(3, 3, 64, 16);
+ line(0, 0, 100, 0);
+ pop();
+
+ stroke(77, 4, 9);
+ let end2 = map(min, 0, 60, 0, 360);
+ arc(0, 0, 350, 350, 0, end2);
+
+ push();
+ rotate(end2);
+ stroke(77, 4, 9, 16);
+ line(0, 0, 70, 0);
+ pop();
+
+ stroke(77, 4, 9);
+ let end3 = map(hr, 0, 12, 0, 360);
+ arc(0, 0, 250, 250, 0, end3);
+
+ push();
+ rotate(end3);
+ stroke(77, 5, 54, 16);
+ line(0, 0, 80, 0);
+ pop();
 
 }
 
@@ -60,6 +103,8 @@ function keyTyped() {
   }
 
     }
+
+
 
 
 
